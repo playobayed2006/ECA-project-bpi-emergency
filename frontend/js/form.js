26b17faeb1340c7e4ca43faa04b7442c30e041d9
obayed <playobayed2006@gmail.com>
2026-02-26 17:20:42 +0600

    Update form.js for render

--- frontend/js/form.js
+++ frontend/js/form.js
@@ -1,7 +1,7 @@
 // --- SMART URL LOGIC ---
 const BASE_URL = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
   ? "http://localhost:3000" 
-  : "https://your-backend-name.onrender.com"; // <--- REPLACE THIS WITH YOUR RENDER URL
+  : "https://eca-project-bpi-emergency.onrender.com/"; // <--- REPLACE THIS WITH YOUR RENDER URL
 // -----------------------
 
 function showPopup(message, loading = false) {
